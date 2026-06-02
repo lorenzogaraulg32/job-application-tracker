d1921a22e46c8b0a78c600d102e65a94f4f65130
Lorenzo Garau <lorenzo.garau.lg32@gmail.com>
2026-06-02 18:14:48 +0200

    Created base React structure, updated the icon, and some basic stuff just to get started

---
+++ frontend-React/src/main.jsx
@@ -0,0 +1,11 @@
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
+
+
+createRoot(document.getElementById('root')).render(
+    <StrictMode>
+        <header>
+            <h1>Ciao ho avviato il programma frontend!</h1>
+        </header>
+    </StrictMode>,
+)
